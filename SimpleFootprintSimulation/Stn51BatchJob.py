8bdbdafba27856990bbc221d29ec1b69777b39f7
rricesmith <rricesmith@gmail.com>
2024-03-06 11:26:03 -0800

    New simple simulation code for footprints added. Configured for Station 51 using IceTop simulation set.

---
+++ SimpleFootprintSimulation/Stn51BatchJob.py
@@ -0,0 +1,22 @@
+import A00_SlurmUtil
+import numpy as np
+
+
+
+n_cores = 1000
+num_icetop = 30
+amp = True
+add_noise = True
+output_folder = 'SimpleFootprintSimulation/output/3.5.24/'
+output_filename = 'Stn51_IceTop'
+
+min_energy = 16.0
+max_energy = 18.6
+
+e_range = np.arange(min_energy, max_energy, 0.1)
+
+
+for e in e_range:
+    cmd = f'python SimpleFootprintSimulation/Stn51Simulation.py {output_folder}{output_filename}_{e:.1f}-{e+0.1:.1f}eV_{n_cores}cores.nur {n_cores} --min_energy {e:.1f} --max_energy {e+0.1:.1f} --num_icetop {num_icetop} --sim_amp {amp} --add_noise {add_noise}'
+
+    A00_SlurmUtil.makeAndRunJob(cmd, f'Stn51_{e:.1f}to{e+0.1:.1f}', runDirectory='run/SimpleFootprintSimulation', partition='standard')
---
+++ SimpleFootprintSimulation/Stn51Simulation.py
@@ -0,0 +1,246 @@
+from NuRadioReco.utilities import units
+import NuRadioReco.modules.io.coreas.readCoREAS
+import NuRadioReco.modules.io.coreas.simulationSelector
+import NuRadioReco.modules.efieldToVoltageConverter
+import NuRadioReco.modules.channelGenericNoiseAdder
+import NuRadioReco.modules.channelBandPassFilter
+import NuRadioReco.modules.electricFieldBandPassFilter
+import NuRadioReco.modules.eventTypeIdentifier
+import NuRadioReco.modules.channelStopFilter
+import NuRadioReco.modules.channelResampler
+import NuRadioReco.modules.trigger.highLowThreshold
+import NuRadioReco.modules.trigger.simpleThreshold
+import NuRadioReco.modules.ARIANNA.hardwareResponseIncorporator
+import NuRadioReco.modules.channelAddCableDelay
+import NuRadioReco.modules.channelLengthAdjuster
+import NuRadioReco.modules.triggerTimeAdjuster
+import astropy
+import argparse
+import NuRadioReco.modules.io.eventWriter
+import numpy as np
+import os
+import datetime
+from icecream import ic
+from scipy import constants
+
+from NuRadioReco.detector import detector
+from NuRadioReco.detector import generic_detector
+
+import logging
+logger=logging.getLogger("module")
+logger.setLevel(logging.WARNING)
+
+
+def calculateNoisePerChannel(det, station, amp=True):
+    """
+    Calculate the noise per channel
+        det: detector object
+        station: station object
+        passband: passband settings for trigger
+        amp: boolean for whether to include amplifier
+    """
+    # Setup conditions for amplifier. Assuming a 200s amplifier
+    noise_figure = 1.4
+    noise_temp = 400 * units.kelvin
+    passband = [50*units.MHz, 1000*units.MHz, 'butter', 2]
+
+    preAmpVrms_per_channel = {}
+    postAmpVrms_per_channel = {}
+    for channel_id in station.get_channel_ids():
+        channel = station.get_channel(channel_id)
+        sim_sampling_rate = channel.get_sampling_rate()
+
+        dt = 1 / sim_sampling_rate
+        ff = np.linspace(0, 0.5/dt /units.GHz, 10000)
+        filt = np.ones_like(ff, dtype=complex)
+
+        if amp:
+            filt = hardwareResponseIncorporator.get_filter(ff, station.get_id(), channel_id, det, sim_to_data=True)
+        else:
+            filt *= channelBandPassFilter.get_filter(ff, station.get_id(), channel_id, det, passband=[passband[0], passband[1]], filter_type=passband[2], order=passband[3], rp=0.1)
+
+        bandwidth = np.trapz(np.abs(filt)**2, ff)
+        Vrms = (noise_temp * 50 * constants.k * bandwidth/units.Hz * noise_figure) **0.5
+
+        if amp:
+            max_freq = 0.5 / dt
+            preAmpVrms_per_channel[channel_id] = Vrms / (bandwidth / max_freq)**0.5
+            postAmpVrms_per_channel[channel_id] = Vrms
+        else:
+            preAmpVrms_per_channel[channel_id] = Vrms
+            postAmpVrms_per_channel[channel_id] = Vrms
+
+    return preAmpVrms_per_channel, postAmpVrms_per_channel
+
+
+def pullFilesForSimulation(sim_type, min_energy, max_energy, num_icetop=10, icetop_sin=-1):
+    """
+    Pull in IceTop files for simulation
+    IceTop simulations range from 16.0-18.5 log10eV
+    Sin^2(zenith) bins range from 0.0-1.0
+    There are ~33 separate footprints per Energy/Sin^2 bin
+    """
+
+    input_files = []
+    if min_energy < 16.0:
+        print(f'Setting IceTop min energy to 16.0 log10eV')
+        min_energy = 16.0
+    if max_energy == -1 or max_energy > 18.5:
+        print(f'Setting IceTop max energy to 18.5 log10eV')
+        max_energy = 18.5
+
+    i = min_energy
+    while i < max_energy:
+        #Currently just iterating through all sin's equally. Can separate sin bins if needed
+        if icetop_sin == -1:
+            sin2Val = np.arange(0, 1.01, 0.1)
+        else:
+            sin2Val = [icetop_sin]
+        for sin2 in sin2Val:
+            num_in_bin = 0
+            folder = f'../../../../../dfs8/sbarwick_lab/arianna/SIM/southpole/IceTop/lgE_{i:.1f}/sin2_{sin2:.1f}/'
+            for (dirpath, dirnames, filenames) in os.walk(folder):
+                for file in filenames:
+                    if not 'highlevel' in file:
+                        file = os.path.join(folder, file)
+                        input_files.append(file)
+                        num_in_bin += 1
+                    if num_in_bin == num_icetop:
+                        continue
+        i += 0.1
+
+    return input_files
+
+# Read in settings for simulation
+parser = argparse.ArgumentParser(description='Run Cosmic Ray simulation for Station 51')
+parser.add_argument('output_filename', type=str, help='Output filename for simulation')
+parser.add_argument('n_cores', type=int, help='Number of cores to use for simulation')
+parser.add_argument('--min_energy', type=float, default=16.0, help='Minimum energy for simulation')
+parser.add_argument('--max_energy', type=float, default=18.5, help='Maximum energy for simulation')
+parser.add_argument('--num_icetop', type=int, default=10, help='Number of IceTop footprints to simulate per bin')
+parser.add_argument('--sim_amp', default=True, help='Include amplifier in simulation')
+parser.add_argument('--add_noise', default=False, help='Include noise in simulation')
+
+args = parser.parse_args()
+output_filename = args.output_filename
+n_cores = args.n_cores
+min_energy = args.min_energy
+max_energy = args.max_energy
+num_icetop = args.num_icetop
+sim_amp = args.sim_amp
+add_noise = args.add_noise
+
+# Get files for simulation
+input_files = pullFilesForSimulation('IceTop', min_energy, max_energy, num_icetop=num_icetop, icetop_sin=-1)
+
+# Setup detector
+# det = detector.Detector(json_filename=f'configurations/station51_InfAir.json', assume_inf=False, antenna_by_depth=False)
+det = detector.Detector(json_filename=f'configurations/station51_InfAir.json', assume_inf=False, antenna_by_depth=False)
+det.update(astropy.time.Time('2018-1-1'))
+station_id = 51
+
+# Indicate channels for simulation
+direct_LPDA_channels = [4, 5, 6]
+sigma_trigger = 5 # Sigma threshold to trigger on
+
+# initialize all modules that are needed for processing
+# provide input parameters that are to remain constant during processung
+readCoREAS = NuRadioReco.modules.io.coreas.readCoREAS.readCoREAS()
+readCoREAS.begin(input_files, station_id, n_cores=n_cores, max_distance=None)
+
+simulationSelector = NuRadioReco.modules.io.coreas.simulationSelector.simulationSelector()
+simulationSelector.begin()
+
+efieldToVoltageConverter = NuRadioReco.modules.efieldToVoltageConverter.efieldToVoltageConverter()
+efieldToVoltageConverter.begin(debug=False)
+
+hardwareResponseIncorporator = NuRadioReco.modules.ARIANNA.hardwareResponseIncorporator.hardwareResponseIncorporator()
+
+channelGenericNoiseAdder = NuRadioReco.modules.channelGenericNoiseAdder.channelGenericNoiseAdder()
+channelGenericNoiseAdder.begin()
+
+simpleThreshold = NuRadioReco.modules.trigger.simpleThreshold.triggerSimulator()
+highLowThreshold = NuRadioReco.modules.trigger.highLowThreshold.triggerSimulator()
+
+channelBandPassFilter = NuRadioReco.modules.channelBandPassFilter.channelBandPassFilter()
+channelBandPassFilter.begin()
+
+eventTypeIdentifier = NuRadioReco.modules.eventTypeIdentifier.eventTypeIdentifier()
+channelStopFilter = NuRadioReco.modules.channelStopFilter.channelStopFilter()
+
+channelResampler = NuRadioReco.modules.channelResampler.channelResampler()
+channelResampler.begin()
+
+triggerTimeAdjuster = NuRadioReco.modules.triggerTimeAdjuster.triggerTimeAdjuster()
+triggerTimeAdjuster.begin(trigger_name=f'direct_LPDA_2of3_{sigma_trigger}sigma')
+
+
+eventWriter = NuRadioReco.modules.io.eventWriter.eventWriter()
+eventWriter.begin(output_filename)
+
+preAmpVrms_per_channel = {}
+
+# Start simulation
+for iE, evt in enumerate(readCoREAS.run(detector=det)):
+    logger.info("processing event {:d} with id {:d}".format(iE, evt.get_id()))
+
+    # for station in evt.get_stations():
+
+    station = evt.get_station(station_id)
+    station.set_station_time(datetime.datetime(2018, 10, 1))
+    det.update(station.get_station_time())
+
+    eventTypeIdentifier.run(evt, station, mode='forced', forced_event_type='cosmic_ray')
+    # channelAddCableDelay.run(evt, station, det, mode='add')   # Not sure if necessary
+    efieldToVoltageConverter.run(evt, station, det)
+    channelResampler.run(evt, station, det, 2*units.GHz)
+
+
+    if preAmpVrms_per_channel == {}:
+        # Get noise levels for simulation
+        preAmpVrms_per_channel, postAmpVrms_per_channel = calculateNoisePerChannel(det, station=station, amp=sim_amp)
+        if add_noise:
+            threshold_high = {key: value * sigma_trigger for key, value in postAmpVrms_per_channel.items()}
+            threshold_low = {key: value * -sigma_trigger for key, value in postAmpVrms_per_channel.items()}
+        else:
+            threshold_high = {key: value * sigma_trigger for key, value in preAmpVrms_per_channel.items()}
+            threshold_low = {key: value * -sigma_trigger for key, value in preAmpVrms_per_channel.items()}
+
+        # ic(preAmpVrms_per_channel, postAmpVrms_per_channel, threshold_high, threshold_low)
+
+
+    if simulationSelector.run(evt, station.get_sim_station(), det):
+
+        efieldToVoltageConverter.run(evt, station, det)
+
+        if add_noise:
+            channelGenericNoiseAdder.run(evt, station, det, type='rayleigh', amplitude=preAmpVrms_per_channel)
+
+        hardwareResponseIncorporator.run(evt, station, det, sim_to_data=True)
+
+        highLowThreshold.run(evt, station, det, threshold_high=threshold_high, 
+                            threshold_low=threshold_low,
+                            coinc_window = 40*units.ns,
+                            triggered_channels=direct_LPDA_channels,
+                            number_concidences=2,
+                            trigger_name=f'direct_LPDA_2of3_{sigma_trigger}sigma')
+
+
+        if station.get_trigger(f'direct_LPDA_2of3_{sigma_trigger}sigma').has_triggered():
+
+            highLowThreshold.run(evt, station, det, threshold_high=threshold_high, 
+                                threshold_low=threshold_low,
+                                coinc_window = 40*units.ns,
+                                triggered_channels=direct_LPDA_channels,
+                                number_concidences=2,
+                                trigger_name=f'direct_LPDA_3of3_{sigma_trigger}sigma')
+
+
+            triggerTimeAdjuster.run(evt, station, det)
+            channelResampler.run(evt, station, det, 2*units.GHz)
+            channelStopFilter.run(evt, station, det)
+            eventWriter.run(evt, det)
+
+
+nevents = eventWriter.end()
+print("Finished processing, {} events".format(nevents))
