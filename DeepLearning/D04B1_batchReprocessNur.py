4f944a2cf6635ab3f97ca79c71e537fc5b091930
rricesmith <rricesmith@gmail.com>
2024-10-25 13:36:37 -0700

    Allow for batch D04B1 to not do all files separately

--- DeepLearning/D04B1_batchReprocessNur.py
+++ DeepLearning/D04B1_batchReprocessNur.py
@@ -3,6 +3,7 @@ import os
 
 
 base_folder = '7thpass'
+single_file = False
 
 stations_100s = [13, 15, 18, 32]
 stations_200s = [14, 17, 19, 30]
@@ -14,15 +15,20 @@ for station in stations_100s:
     folder = f'{base_folder}/Station{station}'
     station_path = f"/dfs8/sbarwick_lab/ariannaproject/station_nur/station_{station}/"
     i = 0
-    for file in os.listdir(station_path):
-        if file.endswith('_statDatPak.root.nur'):
-            continue    
-        else:
-            filename = os.path.join(station_path, file)
-            cmd = f'python DeepLearning/D04B_reprocessNurPassingCut.py {station} --folder {folder} --single_file {filename} --amp {amp}'
-            print(f'cmd {cmd}')
-            slurm.makeAndRunJob(cmd, jobName=f'Stn{station}_{i}', partition='standard', runDirectory='run/')
-            i += 1
+    if single_file:
+        for file in os.listdir(station_path):
+            if file.endswith('_statDatPak.root.nur'):
+                continue    
+            else:
+                filename = os.path.join(station_path, file)
+                cmd = f'python DeepLearning/D04B_reprocessNurPassingCut.py {station} --folder {folder} --single_file {filename} --amp {amp}'
+                print(f'cmd {cmd}')
+                slurm.makeAndRunJob(cmd, jobName=f'Stn{station}_{i}', partition='standard', runDirectory='run/')
+                i += 1
+    else:
+        cmd = f'python DeepLearning/D04B_reprocessNurPassingCut.py {station} --folder {folder} --amp {amp}'
+        print(f'cmd {cmd}')
+        slurm.makeAndRunJob(cmd, jobName=f'Stn{station}_{i}', partition='standard', runDirectory='run/')
 
 quit()
 
@@ -31,14 +37,19 @@ for station  in stations_200s:
     folder = f'{base_folder}/Station{station}'
     station_path = f"/dfs8/sbarwick_lab/ariannaproject/station_nur/station_{station}/"
     i = 0
-    for file in os.listdir(station_path):
-        if file.endswith('_statDatPak.root.nur'):
-            continue    
-        else:
-            filename = os.path.join(station_path, file)
-            cmd = f'python DeepLearning/D04B_reprocessNurPassingCut.py {station} --folder {folder} --single_file {filename} --amp {amp}'
-            slurm.makeAndRunJob(cmd, jobName=f'Stn{station}_{i}', partition='standard', runDirectory='run/')
-            i += 1
+    if single_file:
+        for file in os.listdir(station_path):
+            if file.endswith('_statDatPak.root.nur'):
+                continue    
+            else:
+                filename = os.path.join(station_path, file)
+                cmd = f'python DeepLearning/D04B_reprocessNurPassingCut.py {station} --folder {folder} --single_file {filename} --amp {amp}'
+                slurm.makeAndRunJob(cmd, jobName=f'Stn{station}_{i}', partition='standard', runDirectory='run/')
+                i += 1
+    else:
+        cmd = f'python DeepLearning/D04B_reprocessNurPassingCut.py {station} --folder {folder} --amp {amp}'
+        print(f'cmd {cmd}')
+        slurm.makeAndRunJob(cmd, jobName=f'Stn{station}_{i}', partition='standard', runDirectory='run/')
 
 for station in stations_300s:
     amp = '300s'
@@ -46,23 +57,34 @@ for station in stations_300s:
     folder = f'{base_folder}/Station{station}/FirstCh{first_ch}'
     station_path = f"/dfs8/sbarwick_lab/ariannaproject/leshanz_backup/arianna/station_{station}/data/"
     i = 0
-    for file in os.listdir(station_path):
-        if file.endswith('_statDatPak.root.nur'):
-            continue    
-        else:
-            filename = os.path.join(station_path, file)
-            cmd = f'python DeepLearning/D04B_reprocessNurPassingCut.py {station} --folder {folder} --single_file {filename} --amp {amp} --first_ch {first_ch}'
-            slurm.makeAndRunJob(cmd, jobName=f'Stn{station}_{i}', partition='standard', runDirectory='run/')
-            i += 1
+    if single_file:
+        for file in os.listdir(station_path):
+            if file.endswith('_statDatPak.root.nur'):
+                continue    
+            else:
+                filename = os.path.join(station_path, file)
+                cmd = f'python DeepLearning/D04B_reprocessNurPassingCut.py {station} --folder {folder} --single_file {filename} --amp {amp} --first_ch {first_ch}'
+                slurm.makeAndRunJob(cmd, jobName=f'Stn{station}_{i}', partition='standard', runDirectory='run/')
+                i += 1
+    else:
+        cmd = f'python DeepLearning/D04B_reprocessNurPassingCut.py {station} --folder {folder} --amp {amp} --first_ch {first_ch}'
+        print(f'cmd {cmd}')
+        slurm.makeAndRunJob(cmd, jobName=f'Stn{station}_{i}', partition='standard', runDirectory='run/')
 
-    i = 0
     first_ch = 4 # 4 is first channel for upward facing LPDAs, only in stn 52
     folder = f'{base_folder}/Station{station}/FirstCh{first_ch}'
-    for file in os.listdir(station_path):
-        if file.endswith('_statDatPak.root.nur'):
-            continue    
-        else:
-            filename = os.path.join(station_path, file)
-            cmd = f'python DeepLearning/D04B_reprocessNurPassingCut.py {station} --folder {folder} --single_file {filename} --amp {amp} --first_ch {first_ch}'
-            slurm.makeAndRunJob(cmd, jobName=f'Stn{station}_{i}', partition='standard', runDirectory='run/')
-            i += 1
+    if single_file:
+        i = 0
+        for file in os.listdir(station_path):
+            if file.endswith('_statDatPak.root.nur'):
+                continue    
+            else:
+                filename = os.path.join(station_path, file)
+                cmd = f'python DeepLearning/D04B_reprocessNurPassingCut.py {station} --folder {folder} --single_file {filename} --amp {amp} --first_ch {first_ch}'
+                slurm.makeAndRunJob(cmd, jobName=f'Stn{station}_{i}', partition='standard', runDirectory='run/')
+                i += 1
+    else:
+        cmd = f'python DeepLearning/D04B_reprocessNurPassingCut.py {station} --folder {folder} --amp {amp} --first_ch {first_ch}'
+        print(f'cmd {cmd}')
+        slurm.makeAndRunJob(cmd, jobName=f'Stn{station}_{i}', partition='standard', runDirectory='run/')
+
