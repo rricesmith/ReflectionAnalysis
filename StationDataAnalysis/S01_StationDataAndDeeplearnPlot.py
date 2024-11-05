ddae4bfbc2af7f7a65d8dbd60f2b8d653959c3ef
rricesmith <rricesmith@gmail.com>
2024-11-05 12:06:04 -0800

    Plot 2016 events on Chi-SNR plots

--- StationDataAnalysis/S01_StationDataAndDeeplearnPlot.py
+++ StationDataAnalysis/S01_StationDataAndDeeplearnPlot.py
@@ -11,172 +11,163 @@ import DeepLearning.D04C_CutInBacklobeRCR as D04C_CutInBacklobeRCR
 from StationDataAnalysis.S00_FoundEventsSearchUtil import inStation2016
 
 
+def set_CHI_SNR_axis(ax, station_id):
+
+    ax.set_xlim((3, 100))
+    ax.set_ylim((0, 1))
+    ax.set_xlabel('SNR')
+    ax.set_xscale('log')
+    ax.tick_params(axis='x', which='minor', bottom=True)
+    ax.grid(visible=True, which='both', axis='both')
+    ax.set_title(f'Station {station_id}')
+    ax.legend()
+    ax.set_ylabel('Avg Chi Highest Parallel Channels')
+    return ax
+
 datapass = '7thpass'
 stations_100s = [13, 15, 18, 32]
 stations_200s = [14, 17, 19, 30]
+stations = {100: stations_100s, 200: stations_200s}
 
 station_id = 30
 series = 200
 
-if series == 200:
-    noiseRMS = 22.53 * units.mV
-elif series == 100:
-    # Look up
-    # noiseRMS = 15.8
-    i = 0
 
-data_to_plot = []
-
-data_to_plot.append(f'DeepLearning/data/{datapass}/Station{station_id}/FilteredStation{station_id}_Data_SNR_Chi.npy')
-plotfolder = f'StationDataAnalysis/plots/Station_{station_id}'
-if not os.path.exists(plotfolder):
-    os.makedirs(plotfolder)
+for series in stations.keys():
+    if series == 200:
+        noiseRMS = 22.53 * units.mV
+    elif series == 100:
+        # Look up
+        noiseRMS = 20.0 * units.mV
+    for station_id in stations[series]:
 
 
-# First plot is just station data, old vs new chi
-fig, axs = plt.subplots(1, len(data_to_plot), sharey=True, facecolor='w')
-axs = np.atleast_1d(axs)
+        station_data_folder = f'DeepLearning/data/{datapass}/Station{station_id}'
 
-data = np.load(data_to_plot[0], allow_pickle=True)
+        data = np.load(f'{station_data_folder}/FilteredStation{station_id}_Data_SNR_Chi.npy', allow_pickle=True)
+        data_SnrChiCut = np.load(f'{station_data_folder}/FilteredStation{station_id}_Data_SnrChiCut.npy', allow_pickle=True)
+        data_in2016 = np.load(f'{station_data_folder}/FilteredStation{station_id}_Data_In2016.npy', allow_pickle=True)
 
-All_SNRs, All_RCR_Chi, PassingCut_SNRs, PassingCut_RCR_Chi, PassingCut_Azi, PassingCut_Zen = data
-
-SNRbins = np.logspace(0.477, 2, num=80)
-maxCorrBins = np.arange(0, 1.0001, 0.01)
-
-
-templates_RCR = D00_helperFunctions.loadMultipleTemplates(series)
-templates_RCR.append(D00_helperFunctions.loadSingleTemplate(series))
-# templates_RCR = [templates_RCR]
-
-axs[0].hist2d(All_SNRs, All_RCR_Chi, bins=[SNRbins, maxCorrBins], norm=matplotlib.colors.LogNorm(), label=f'{np.sum(D04C_CutInBacklobeRCR.RCRChiSNRCutMask(All_SNRs, All_RCR_Chi))} Events Passing Cut')        
-D04C_CutInBacklobeRCR.plotRCRChiSNRCut(ax=axs[0], label=True)
-
-axs[0].set_xlim((3, 100))
-axs[0].set_ylim((0, 1))
-axs[0].set_xlabel('SNR')
-axs[0].set_xscale('log')
-axs[0].tick_params(axis='x', which='minor', bottom=True)
-axs[0].grid(visible=True, which='both', axis='both')
-axs[0].set_title(f'Station {station_id}')
-axs[0].legend()
-
-axs[0].set_ylabel('Avg Chi Highest Parallel Channels')
-
-savename = f'{plotfolder}/ChiSNR_Stn{station_id}.png'
-fig.savefig(savename)
-print(f'Saved {savename}')
-# quit()
-
-# Second plot is station data with simulated air showers
-fig2, axs2 = plt.subplots(1, len(data_to_plot), sharey=True, facecolor='w')
-axs2 = np.atleast_1d(axs2)
+        plotfolder = f'StationDataAnalysis/plots/Station_{station_id}'
+        if not os.path.exists(plotfolder):
+            os.makedirs(plotfolder)
 
 
 
-# templates_RCR = D00_helperFunctions.loadMultipleTemplates(series)
-# templates_RCR.append(D00_helperFunctions.loadSingleTemplate(series))
+        # Load data and set plot bins
+        SNRbins = np.logspace(0.477, 2, num=80)
+        maxCorrBins = np.arange(0, 1.0001, 0.01)
+
+        All_SNRs, All_RCR_Chi, PassingCut_SNRs, PassingCut_RCR_Chi, PassingCut_Azi, PassingCut_Zen = data
+        in2016_SNRs, in2016_RCR_Chi, in2016_Azi, in2016_Zen, in2016_Traces, in2016_Times = data_in2016
 
 
-plotSimSNRChi(templates_RCR, noiseRMS, ax=axs2[0], cut=True, path=f'DeepLearning/data/3rdpass/')
-plotSimSNRChi(templates_RCR, noiseRMS, ax=axs[0], cut=True, path=f'DeepLearning/data/3rdpass/')
+        templates_RCR = D00_helperFunctions.loadMultipleTemplates(series)
+        templates_RCR.append(D00_helperFunctions.loadSingleTemplate(series))
 
-D04C_CutInBacklobeRCR.plotRCRChiSNRCut(ax=axs[0], label=False)
-D04C_CutInBacklobeRCR.plotRCRChiSNRCut(ax=axs2[0], label=True)
+        ############################################################################################
+        # First plot is only station data, Chi v SNR
+        fig, axs = plt.subplots(1, len(data_to_plot), sharey=True, facecolor='w')
+        axs = np.atleast_1d(axs)
 
+        axs[0].hist2d(All_SNRs, All_RCR_Chi, bins=[SNRbins, maxCorrBins], norm=matplotlib.colors.LogNorm(), label=f'{np.sum(D04C_CutInBacklobeRCR.RCRChiSNRCutMask(All_SNRs, All_RCR_Chi))} Events Passing Cut')        
+        D04C_CutInBacklobeRCR.plotRCRChiSNRCut(ax=axs[0], label=True)
+        set_CHI_SNR_axis(axs[0], station_id)
 
-axs2[0].set_xlim((3, 100))
-axs2[0].set_ylim((0, 1))
-axs2[0].set_xlabel('SNR')
-axs2[0].set_xscale('log')
-axs2[0].tick_params(axis='x', which='minor', bottom=True)
-axs2[0].grid(visible=True, which='both', axis='both')
-axs[0].set_title(f'Station {station_id}')
-axs2[0].legend()
-
-axs[0].legend()
-
-axs2[0].set_ylabel('Avg Chi Highest Parallel Channels')
+        savename = f'{plotfolder}/ChiSNR_Stn{station_id}.png'
+        fig.savefig(savename)
+        print(f'Saved {savename}')
+        # quit()
 
 
-savename = f'{plotfolder}/ChiSNR_wRCR_Stn{station_id}.png'
-fig.savefig(savename)
-print(f'Saved {savename}')
+        ############################################################################################
+        # Second plot is station data with simulated air showers
+        fig2, axs2 = plt.subplots(1, len(data_to_plot), sharey=True, facecolor='w')
+        axs2 = np.atleast_1d(axs2)
 
-savename = f'{plotfolder}/ChiSNR_RCR_Stn{station_id}.png'
-fig2.savefig(savename)
-print(f'Saved {savename}')
+        plotSimSNRChi(templates_RCR, noiseRMS, ax=axs2[0], cut=True, path=f'DeepLearning/data/3rdpass/')
+        plotSimSNRChi(templates_RCR, noiseRMS, ax=axs[0], cut=True, path=f'DeepLearning/data/3rdpass/')
 
-# Third plot adds backlobe onto total plot, and does separate of backlobe only
-fig2, axs2 = plt.subplots(1, len(data_to_plot), sharey=True, facecolor='w')
-axs2 = np.atleast_1d(axs2)
+        D04C_CutInBacklobeRCR.plotRCRChiSNRCut(ax=axs[0], label=False)
+        D04C_CutInBacklobeRCR.plotRCRChiSNRCut(ax=axs2[0], label=True)
 
+        set_CHI_SNR_axis(axs2[0], station_id)
+        axs[0].legend()
 
-# templates_RCR = D00_helperFunctions.loadMultipleTemplates(series)
-# templates_RCR.append(D00_helperFunctions.loadSingleTemplate(series))
-        
-plotSimSNRChi(templates_RCR, noiseRMS, type='Backlobe', ax=axs[0], cut=True, path=f'DeepLearning/data/3rdpass/')
-plotSimSNRChi(templates_RCR, noiseRMS, type='Backlobe', ax=axs2[0], cut=True, path=f'DeepLearning/data/3rdpass/')
-
-D04C_CutInBacklobeRCR.plotRCRChiSNRCut(ax=axs[0], label=False)
-D04C_CutInBacklobeRCR.plotRCRChiSNRCut(ax=axs2[0], label=True)
-
-
-axs2[0].set_xlim((3, 100))
-axs2[0].set_ylim((0, 1))
-axs2[0].set_xlabel('SNR')
-axs2[0].set_xscale('log')
-axs2[0].tick_params(axis='x', which='minor', bottom=True)
-axs2[0].grid(visible=True, which='both', axis='both')
-axs[0].set_title(f'Station {station_id}')
-axs2[0].legend()
-
-axs2[0].set_ylabel('Avg Chi Highest Parallel Channels')
+        savename = f'{plotfolder}/ChiSNR_wRCR_Stn{station_id}.png'
+        fig.savefig(savename)
+        print(f'Saved {savename}')
+        savename = f'{plotfolder}/ChiSNR_RCR_Stn{station_id}.png'
+        fig2.savefig(savename)
+        print(f'Saved {savename}')
 
 
-savename = f'{plotfolder}/ChiSNR_wRCR_wBacklobe_Stn{station_id}.png'
-fig.savefig(savename)
-print(f'Saved {savename}')
+        ############################################################################################
+        # Third plot adds backlobe onto total plot, and does separate of backlobe only
+        fig2, axs2 = plt.subplots(1, len(data_to_plot), sharey=True, facecolor='w')
+        axs2 = np.atleast_1d(axs2)
+                
+        plotSimSNRChi(templates_RCR, noiseRMS, type='Backlobe', ax=axs[0], cut=True, path=f'DeepLearning/data/3rdpass/')
+        plotSimSNRChi(templates_RCR, noiseRMS, type='Backlobe', ax=axs2[0], cut=True, path=f'DeepLearning/data/3rdpass/')
 
-savename = f'{plotfolder}/ChiSNR_Backlobe_Stn{station_id}.png'
-fig2.savefig(savename)
-print(f'Saved {savename}')
+        D04C_CutInBacklobeRCR.plotRCRChiSNRCut(ax=axs[0], label=False)
+        D04C_CutInBacklobeRCR.plotRCRChiSNRCut(ax=axs2[0], label=True)
 
-# Fourth plot shows the selected events from Time-ML cut
-fig2, axs2 = plt.subplots(1, len(data_to_plot), sharey=True, facecolor='w')
-axs2 = np.atleast_1d(axs2)
+        set_CHI_SNR_axis(axs2[0], station_id)
 
-
-# templates_RCR = D00_helperFunctions.loadMultipleTemplates(series)
-# templates_RCR.append(D00_helperFunctions.loadSingleTemplate(series))
-
-axs[0].scatter(PassingCut_SNRs, PassingCut_RCR_Chi, label=f'{len(PassingCut_RCR_Chi)} Events Passing Time-ML Cut', facecolor='none', edgecolor='black')
-axs2[0].scatter(PassingCut_SNRs, PassingCut_RCR_Chi, label=f'{len(PassingCut_RCR_Chi)} Events Passing Time-ML Cut', facecolor='none', edgecolor='black')
-
-D04C_CutInBacklobeRCR.plotRCRChiSNRCut(ax=axs[0], label=False)
-D04C_CutInBacklobeRCR.plotRCRChiSNRCut(ax=axs2[0], label=True)
+        savename = f'{plotfolder}/ChiSNR_wRCR_wBacklobe_Stn{station_id}.png'
+        fig.savefig(savename)
+        print(f'Saved {savename}')
+        savename = f'{plotfolder}/ChiSNR_Backlobe_Stn{station_id}.png'
+        fig2.savefig(savename)
+        print(f'Saved {savename}')
 
 
-axs2[0].set_xlim((3, 100))
-axs2[0].set_ylim((0, 1))
-axs2[0].set_xlabel('SNR')
-axs2[0].set_xscale('log')
-axs2[0].tick_params(axis='x', which='minor', bottom=True)
-axs2[0].grid(visible=True, which='both', axis='both')
-axs[0].set_title(f'Station {station_id}')
-axs2[0].legend()
+        ############################################################################################
+        # Fourth plot shows the selected events from Time-ML cut
+        fig2, axs2 = plt.subplots(1, len(data_to_plot), sharey=True, facecolor='w')
+        axs2 = np.atleast_1d(axs2)
 
-axs2[0].set_ylabel('Avg Chi Highest Parallel Channels')
+        axs[0].scatter(PassingCut_SNRs, PassingCut_RCR_Chi, label=f'{len(PassingCut_RCR_Chi)} Events Passing Time-ML Cut', facecolor='none', edgecolor='black')
+        axs2[0].scatter(PassingCut_SNRs, PassingCut_RCR_Chi, label=f'{len(PassingCut_RCR_Chi)} Events Passing Time-ML Cut', facecolor='none', edgecolor='black')
+
+        D04C_CutInBacklobeRCR.plotRCRChiSNRCut(ax=axs[0], label=False)
+        D04C_CutInBacklobeRCR.plotRCRChiSNRCut(ax=axs2[0], label=True)
+
+        set_CHI_SNR_axis(axs2[0], station_id)
 
 
-savename = f'{plotfolder}/ChiSNR_wRCR_wBacklobe_wSelected_Stn{station_id}.png'
-fig.savefig(savename)
-print(f'Saved {savename}')
+        savename = f'{plotfolder}/ChiSNR_wRCR_wBacklobe_wSelected_Stn{station_id}.png'
+        fig.savefig(savename)
+        print(f'Saved {savename}')
 
-savename = f'{plotfolder}/ChiSNR_Selected_Stn{station_id}.png'
-fig2.savefig(savename)
-print(f'Saved {savename}')
+        savename = f'{plotfolder}/ChiSNR_Selected_Stn{station_id}.png'
+        fig2.savefig(savename)
+        print(f'Saved {savename}')
 
-fig2, axs2 = plt.subplots(1, len(data_to_plot), sharey=True, facecolor='w')
 
-# fig.colorbar()
+        ############################################################################################
+        # Fifth plot shows events from 2016
+        fig2, axs2 = plt.subplots(1, len(data_to_plot), sharey=True, facecolor='w')
+        axs2 = np.atleast_1d(axs2)
+
+        axs[0].scatter(in2016_SNRs, in2016_RCR_Chi, label=f'{len(in2016_RCR_Chi)} Events from 2016', marker='*', facecolor='red', edgecolor='black')
+        axs2[0].scatter(in2016_SNRs, in2016_RCR_Chi, label=f'{len(in2016_RCR_Chi)} Events from 2016', marker='*', facecolor='red', edgecolor='black')
+
+        D04C_CutInBacklobeRCR.plotRCRChiSNRCut(ax=axs[0], label=False)
+        D04C_CutInBacklobeRCR.plotRCRChiSNRCut(ax=axs2[0], label=True)
+
+        set_CHI_SNR_axis(axs2[0], station_id)
+
+        savename = f'{plotfolder}/ChiSNR_wRCR_wBacklobe_wSelected_w2016_Stn{station_id}.png'
+        fig.savefig(savename)
+        print(f'Saved {savename}')
+
+        savename = f'{plotfolder}/ChiSNR_Selected_w2016_Stn{station_id}.png'
+        fig2.savefig(savename)
+        print(f'Saved {savename}')
+
+        ############################################################################################
+
+
+    plt.close('all')
