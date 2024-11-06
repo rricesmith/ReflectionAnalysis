7c96a3c9ea61e0f60bd94e435a46f9b327d93fa8
rricesmith <rricesmith@gmail.com>
2024-11-06 13:40:46 -0800

    Edit timestrip to do multiple years and sets of data

--- StationDataAnalysis/S02_StationDataAndDeeplearnTimeStrip.py
+++ StationDataAnalysis/S02_StationDataAndDeeplearnTimeStrip.py
@@ -92,14 +92,14 @@ if __name__ == "__main__":
             data_in2016 = np.load(f'{station_data_folder}/FilteredStation{station_id}_Data_In2016.npy', allow_pickle=True)
             times = np.load(f'{station_data_folder}/FilteredStation{station_id}_Data_Times.npy', allow_pickle=True)
 
-            plotfolder = f'StationDataAnalysis/plots/Station_{station_id}'
+            plotfolder = f'StationDataAnalysis/plots/Station_{station_id}/TimeStrips'
             if not os.path.exists(plotfolder):
                 os.makedirs(plotfolder)
 
             # Load all the data
             All_SNRs, All_RCR_Chi, MLCut_SNRs, MLCut_RCR_Chi, MLCut_Azi, MLCut_Zen = data
             ChiCut_SNRS, ChiCut_RCR_Chi, ChiCut_Azi, ChiCut_Zen, ChiCut_Traces = data_SnrChiCut
-            in2016_SNRs, in2016_RCR_Chi, in2016_Azi, in2016_Zen, in2016_Traces, in2016_Times = data_in2016
+            in2016_SNRs, in2016_RCR_Chi, in2016_Azi, in2016_Zen, in2016_Traces, in2016_datetimes = data_in2016
             All_datetimes, MLCut_datetimes, ChiCut_datetimes = times
 
 
@@ -109,18 +109,46 @@ if __name__ == "__main__":
                 MLCut_datetimes = np.vectorize(datetime.datetime.fromtimestamp)(MLCut_datetimes)
             ChiCut_datetimes = np.vectorize(datetime.datetime.fromtimestamp)(ChiCut_datetimes)
             if not len(in2016_Times) == 0:
-                in2016_Times = np.vectorize(datetime.datetime.fromtimestamp)(in2016_Times)
-
-            # Plot the data
-            fig, axs = timestripScatter(All_datetimes, All_RCR_Chi, yearStart=2014, yearEnd=2019, legend='All data', marker='o', color='b', markersize=2)
-            timestripScatter(ChiCut_datetimes, ChiCut_RCR_Chi, yearStart=2014, yearEnd=2019, legend='Chi Cut', marker='o', color='r', markersize=4, fig=fig, axs=axs)
-            plt.legend()
-            fig.suptitle(f'Station {station_id} All Data')
-            savename = f'{plotfolder}/Station{station_id}_Timestrip_AllData.png'
-            plt.savefig(savename, format='png')
-            ic(f'Saved {savename}')
-            plt.close(fig)
+                in2016_datetimes = np.vectorize(datetime.datetime.fromtimestamp)(in2016_datetimes)
 
 
+            # Iterate through each year, then also plot all years at once
+            years = [2014, 2015, 2016, 2017, 2018, 2019]
+            for iY in range(len(years)):
+                if iY == len(years)-1:
+                    yStart = years[0]
+                    yEnd = years[-1]
+                else:
+                    yStart = years[iY]
+                    yEnd = years[iY+1]
+
+                # Plot the data
+                fig, axs = timestripScatter(All_datetimes, All_RCR_Chi, yearStart=yStart, yearEnd=yEnd, legend='All data', marker='o', color='b', markersize=2)
+                plt.legend()
+                fig.suptitle(f'Station {station_id})')
+                savename = f'{plotfolder}/Station{station_id}_Timestrip_AllData_{yStart}-{yEnd}.png'
+                plt.savefig(savename, format='png')
+                ic(f'Saved {savename}')
+
+                timestripScatter(MLCut_datetimes, MLCut_RCR_Chi, yearStart=yEnd, yearEnd=yEnd, legend='ML Cut', marker='o', color='r', markersize=2, fig=fig, axs=axs)
+                plt.legend()
+                savename = f'{plotfolder}/Station{station_id}_Timestrip_MLCut_{yStart}-{yEnd}.png'
+                plt.savefig(savename, format='png')
+                ic(f'Saved {savename}')
+
+                timestripScatter(ChiCut_datetimes, ChiCut_RCR_Chi, yearStart=yEnd, yearEnd=yEnd, legend='Chi Cut', marker='o', color='g', markersize=2, fig=fig, axs=axs)
+                plt.legend()
+                savename = f'{plotfolder}/Station{station_id}_Timestrip_ChiCut_{yStart}-{yEnd}.png'
+                plt.savefig(savename, format='png')
+                ic(f'Saved {savename}')
+
+                timestripScatter(in2016_datetimes, in2016_RCR_Chi, yearStart=yEnd, yearEnd=yEnd, legend='2016 Events', marker='*', color='b', markersize=2, fig=fig, axs=axs)
+                plt.legend()
+                savename = f'{plotfolder}/Station{station_id}_Timestrip_2016Events_{yStart}-{yEnd}.png'
+                plt.savefig(savename, format='png')
+                ic(f'Saved {savename}')
+
+
+                plt.close(fig)
             quit()
 
