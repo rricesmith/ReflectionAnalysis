04627fe32954c16575220a7163b662a0e36e409d
rricesmith <rricesmith@gmail.com>
2025-02-14 06:59:21 -0800

    Debugging histAreaRate

--- HRASimulation/HRAAnalysis.py
+++ HRASimulation/HRAAnalysis.py
@@ -375,15 +375,16 @@ def histAreaRate(x, y, weights, title, savename, colorbar_label='Evts/yr', radiu
 
     fig, ax = plt.subplots()
 
-    h, xedges, yedges = np.histogram2d(x, y, bins=(x_bins, y_bins), weights=weights)
-    # h, xedges, yedges, im = ax.hist2d(x, y, bins=(x_bins, y_bins), weights=weights, cmap=cmap, norm=matplotlib.colors.LogNorm())
-    h, cmap = set_bad_imshow(h, 0)
-    extent = [yedges[0], yedges[-1], xedges[0], xedges[-1]]
-    im = ax.imshow(h, extent=extent, norm=matplotlib.colors.LogNorm(), cmap=cmap)
+    # h, xedges, yedges = np.histogram2d(x, y, bins=(x_bins, y_bins), weights=weights)
+    h, xedges, yedges, im = ax.hist2d(x, y, bins=(x_bins, y_bins), weights=weights, cmap='viridis', norm=matplotlib.colors.LogNorm())
+    # h, cmap = set_bad_imshow(h, 0)
+    # extent = [yedges[0], yedges[-1], xedges[0], xedges[-1]]
+    # im = ax.imshow(h, extent=extent, norm=matplotlib.colors.LogNorm(), cmap=cmap)
 
     ax.set_xlabel('x (km)')
     ax.set_ylabel('y (km)')
-    fig.colorbar(im, ax=ax, label=colorbar_label)
+    # fig.colorbar(im, ax=ax, label=colorbar_label)
+    plt.colorbar(label=colorbar_label)
     ax.set_title(title)
     fig.savefig(savename)
     ic(f'Saved {savename}')
