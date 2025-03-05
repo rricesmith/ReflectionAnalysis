5d0efa792ae6600cbbe388db8c9a8dc5ca506453
rricesmith <rricesmith@gmail.com>
2025-03-04 16:05:18 -0800

    Debugging

--- readCoREASStationGrid.py
+++ readCoREASStationGrid.py
@@ -325,8 +325,7 @@ class readCoREAS:
 
             station_ids = detector.get_station_ids()
             for iCore, core in enumerate(cores):
-                ic(core, self.__area, x, y, r, theta, x_center, y_center)
-                continue
+                ic(core)
                 t = time.time()
                 evt = NuRadioReco.framework.event.Event(self.__current_input_file, iCore)  # create empty event
                 sim_shower = coreas.make_sim_shower(corsika)
