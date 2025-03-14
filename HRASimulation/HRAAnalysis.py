9e1c551e2a9d3e5c5b8a0e3e5796fcaa3131dfa1
rricesmith <rricesmith@gmail.com>
2025-03-14 14:07:08 -0700

    Sign error for azimuth y component

--- HRASimulation/HRAAnalysis.py
+++ HRASimulation/HRAAnalysis.py
@@ -629,13 +629,13 @@ def plotAreaAziZenArrows(x, y, zenith, azimuth, weights, title, savename, dir_tr
             # ic(avg_zen[iX][iY], avg_azi[iX][iY], color, zen_bins, np.digitize(avg_zen[iX][iY], zen_bins))
             # ic(x, y, 0.1*avg_zen[iX][iY]*np.cos(np.deg2rad(avg_azi[iX][iY])), 0.1*avg_zen[iX][iY]*-np.sin(np.deg2rad(avg_azi[iX][iY])))
             # ic(avg_azi[iX][iY], np.cos(np.deg2rad(avg_azi[iX][iY])), -np.sin(np.deg2rad(avg_azi[iX][iY])))
-            ax.arrow(x, y, 4*avg_zen[iX][iY]*np.cos(np.deg2rad(avg_azi[iX][iY])), 4*avg_zen[iX][iY]*-np.sin(np.deg2rad(avg_azi[iX][iY])), head_width=100, head_length=50, color=cmap(avg_zen[iX][iY]/90))
+            ax.arrow(x, y, 4*avg_zen[iX][iY]*np.cos(np.deg2rad(avg_azi[iX][iY])), 4*avg_zen[iX][iY]*np.sin(np.deg2rad(avg_azi[iX][iY])), head_width=100, head_length=50, color=cmap(avg_zen[iX][iY]/90))
 
 
     ax.set_xlim(-max_distance/units.m, max_distance/units.m)
     ax.set_ylim(-max_distance/units.m, max_distance/units.m)
     fig.colorbar(plt.cm.ScalarMappable(norm=matplotlib.colors.Normalize(vmin=0, vmax=90), cmap=cmap), ax=ax, label='Zenith Angle (deg)')
-    ax.legend()
+    ax.legend(loc='upper right')
     ax.set_xlabel('x (m)')
     ax.set_ylabel('y (m)')
     ax.set_title(title)
