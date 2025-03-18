79d92a8a5c604a8062fefc7a6d137d4f315a3afa
rricesmith <rricesmith@gmail.com>
2025-03-18 16:07:09 -0700

    Plot all 2016 traces

--- StationDataAnalysis/S04_PlotTemplateDifferences.py
+++ StationDataAnalysis/S04_PlotTemplateDifferences.py
@@ -65,10 +65,9 @@ if __name__ == "__main__":
                 new_Chi.append(getMaxAllChi(traces, 2*units.GHz, template_series_RCR, 2*units.GHz))
 
             for i, old_chi in enumerate(in2016_RCR_Chi):
-                if new_Chi[i] > old_chi:
-                    ic(new_Chi[i], old_chi)
-                    pT(in2016_Traces[i], in2016_datetimes[i].strftime('%Y-%m-%d %H:%M:%S') + f', {in2016_Azi[i]:.1f}deg Azi, {in2016_Zen[i]:.1f}deg Zen,\n {in2016_SNRs[i]:.1f} SNR, {old_chi:.2f} BL Chi -> {new_Chi[i]:.2f} RCR Chi',
-                       f'{plotfolder}/Station{station_id}_SNR{in2016_SNRs[i]:.2f}_{in2016_datetimes[i]}.png')
+                ic(new_Chi[i], old_chi)
+                pT(in2016_Traces[i], in2016_datetimes[i].strftime('%Y-%m-%d %H:%M:%S') + f', {in2016_Azi[i]:.1f}deg Azi, {in2016_Zen[i]:.1f}deg Zen,\n {in2016_SNRs[i]:.1f} SNR, {old_chi:.2f} BL Chi -> {new_Chi[i]:.2f} RCR Chi',
+                    f'{plotfolder}/Station{station_id}_SNR{in2016_SNRs[i]:.2f}_{in2016_datetimes[i]}.png')
 
             all_2016_SNRs.extend(in2016_SNRs)
             all_2016_RCR_Chi.extend(in2016_RCR_Chi)
