0c217bec5875ac026a386ed1b2056b463f5a5405
rricesmith <rricesmith@gmail.com>
2025-04-09 15:26:13 -0700

    Plotting code for comparing SNR & Chi

--- HRAStationDataAnalysis/plotSNRChiComparisons.py
+++ HRAStationDataAnalysis/plotSNRChiComparisons.py
@@ -2,8 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import argparse
+import gc
+from icecream import ic
 
-
+def setSNRChiPlot(ax):
+    ax.set_xlabel('SNR')
+    ax.set_xscale('log')
+    ax.set_ylabel('Chi')
+    ax.set_ylim(0, 1)
+    ax.set_xlim(3, 100)
+    ax.grid()
 
 if __name__ == "__main__":
     # This code makes the following plots
@@ -38,19 +46,24 @@ if __name__ == "__main__":
         if file.startswith(f'{date}_Station{station_id}_SNR'):
             data = np.load(data_folder+file, allow_pickle=True)
             SNR_array.append(data.tolist())
+            del data
 
         if file.startswith(f'{date}_Station{station_id}_Chi2016'):
             data = np.load(data_folder+file, allow_pickle=True)
             chi_2016_array.append(data.tolist())
+            del data
 
         if file.startswith(f'{date}_Station{station_id}_ChiRCR'):
             data = np.load(data_folder+file, allow_pickle=True)
             chi_RCR_array.append(data.tolist())
+            del data
 
         if file.startswith(f'{date}_Station{station_id}_ChiBad'):
             data = np.load(data_folder+file, allow_pickle=True)
             chi_RCR_bad_array.append(data.tolist())
+            del data
 
+        gc.collect()    # Free memory just in case it's large
 
     # Convert to numpy arrays
     SNR_array = np.array(SNR_array)
@@ -60,3 +73,72 @@ if __name__ == "__main__":
 
 
     # Make the plots
+    # Since 3 sets of data, will plot in a triangular matrix with the SNR-Chi plots on the diagonal
+
+    # 2016 is 0 in both dims
+    # RCR is 1 in both dims
+    # RCR Bad is 2 in both dims
+
+    fig, axs = plt.subplots(3, 3, figsize=(15, 15))
+    fig.suptitle(f'SNR and Chi for station {station_id} on {date}', fontsize=20)
+    fig.subplots_adjust(hspace=0.4, wspace=0.4)
+    # Upper left is SNR vs 2016 Chi
+    axs[0, 0].scatter(SNR_array, chi_2016_array)
+    axs[0, 0].set_title('SNR vs 2016 Chi')
+    setSNRChiPlot(axs[0, 0])
+    # Middle left is Chi 2016 vs RCR Chi
+    axs[1, 0].scatter(chi_2016_array, chi_RCR_array)
+    axs[1, 0].set_title('2016 Chi vs RCR Chi')
+    axs[1, 0].set_xlabel('2016 Chi')
+    axs[1, 0].set_ylabel('RCR Chi')
+    axs[1, 0].set_ylim(0, 1)
+    axs[1, 0].set_xlim(0, 1)
+    # Bottom left is Chi 2016 vs RCR Bad Chi
+    axs[2, 0].scatter(chi_2016_array, chi_RCR_bad_array)
+    axs[2, 0].set_title('2016 Chi vs RCR Bad Chi')
+    axs[2, 0].set_xlabel('2016 Chi')
+    axs[2, 0].set_ylabel('RCR Bad Chi')
+    axs[2, 0].set_ylim(0, 1)
+    axs[2, 0].set_xlim(0, 1)
+
+    # Middle plot is SNR vs RCR Chi
+    axs[1, 1].scatter(SNR_array, chi_RCR_array)
+    axs[1, 1].set_title('SNR vs RCR Chi')
+    setSNRChiPlot(axs[1, 1])
+    # Bottom middle is Chi RCR Chi vs RCR Bad Chi
+    axs[2, 1].scatter(chi_RCR_array, chi_RCR_bad_array)
+    axs[2, 1].set_title('RCR Chi vs RCR Bad Chi')
+    axs[2, 1].set_xlabel('RCR Chi')
+    axs[2, 1].set_ylabel('RCR Bad Chi')
+    axs[2, 1].set_ylim(0, 1)
+    axs[2, 1].set_xlim(0, 1)
+
+    # Bottom right is SNR vs RCR Bad Chi
+    axs[2, 2].scatter(SNR_array, chi_RCR_bad_array)
+    axs[2, 2].set_title('SNR vs RCR Bad Chi')
+    setSNRChiPlot(axs[2, 2])
+
+    # Do the Chi differences on upper triangular matrix
+    # Upper middle is SNR vs (RCR Chi - 2016 Chi)
+    axs[0, 1].scatter(SNR_array, chi_RCR_array - chi_2016_array)
+    axs[0, 1].set_title('SNR vs (RCR Chi - 2016 Chi)')
+    setSNRChiPlot(axs[0, 1])
+    # Upper right is SNR vs (RCR Bad Chi - 2016 Chi)
+    axs[0, 2].scatter(SNR_array, chi_RCR_bad_array - chi_2016_array)
+    axs[0, 2].set_title('SNR vs (RCR Bad Chi - 2016 Chi)')
+    setSNRChiPlot(axs[0, 2])
+    # Middle right is SNR vs (RCR Chi - RCR Bad Chi)
+    axs[1, 2].scatter(SNR_array, chi_RCR_array - chi_RCR_bad_array)
+    axs[1, 2].set_title('SNR vs (RCR Chi - RCR Bad Chi)')
+    setSNRChiPlot(axs[1, 2])
+
+
+    # Save the plots
+    savename = f'{plot_folder}SNR_Chi_Station{station_id}_{date}.png'
+    plt.savefig(savename)
+    ic(f'Saved plot to {savename}')
+    plt.close(fig)
+    # Clear the figure
+    fig.clf()
+    plt.close(fig)
+
