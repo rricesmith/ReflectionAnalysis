b5d23f24c639492dcab71bcbdd617d4b8b64453e
rricesmith <rricesmith@gmail.com>
2025-04-16 19:23:03 -0700

    Debug

--- HRAStationDataAnalysis/newDeepLearnAttempt.py
+++ HRAStationDataAnalysis/newDeepLearnAttempt.py
@@ -30,8 +30,8 @@ for file in os.listdir(station_data_folder):
         print(f"Loading file: {file_path}")
         events = np.load(file_path, allow_pickle=True)
         print(events.shape)
-        print(mask)
         mask = not np.any(events, axis=0, keepdims=True)  # Check if any event is all zero
+        print(mask)
         print(events.shape, mask.shape, np.sum(mask), events[mask].shape)
         if np.sum(mask) == 0:
             print("No events to load.")
