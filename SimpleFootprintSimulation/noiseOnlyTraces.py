8d5ba07df61966ec9abea00858d205b087be404c
rricesmith <rricesmith@gmail.com>
2025-05-08 14:10:35 -0700

    Create noise only traces

---
+++ SimpleFootprintSimulation/noiseOnlyTraces.py
@@ -0,0 +1,41 @@
+import NuRadioReco.modules.channelGenericNoiseAdder as channelGenericNoiseAdder
+from NuRadioReco.utilities import units
+import numpy as np
+
+
+if __name__ == '__main__':
+
+    # Using modules from channelGenericNoiseAdder, will generate pure noise traces
+
+    sampling_rate = 2*units.GHz
+    n_samples = 256
+
+    min_freq = 50*units.MHz
+    max_freq = 2000*units.MHz
+
+    noise_type = 'rayleigh'
+    amplitude = 10*units.mV
+    bandwidth = None
+
+
+    n_noise = 10
+    noise_array = np.zeros((n_noise, n_samples))
+
+    for i in range(n_noise):
+        noise = channelGenericNoiseAdder.bandlimited_noise(min_freq, max_freq, n_samples, sampling_rate, amplitude, noise_type, bandwidth)
+
+        if True:
+            # Plot the noise
+            import matplotlib.pyplot as plt
+            plt.plot(noise[0])
+            plt.title('Noise Trace')
+            plt.xlabel('Sample Number')
+            plt.ylabel('Amplitude (V)')
+            plt.grid()
+            plt.savefig(f'SimpleFootprintSimulation/plots/noise_trace_{i}.png')
+            plt.close()
+        
+        noise_array[i] = noise
+
+    # Save the noise array
+    np.save('SimpleFootprintSimulation/output/noise_array.npy', noise_array)
