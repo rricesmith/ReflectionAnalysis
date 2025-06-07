68337cae17654052bc8bab669f993f4206e43b06
rricesmith <rricesmith@gmail.com>
2025-06-06 20:35:45 -0700

    Update batch submission

--- HRAStationDataAnalysis/batchEventSearchCuts.py
+++ HRAStationDataAnalysis/batchEventSearchCuts.py
@@ -10,15 +10,16 @@ if __name__ == "__main__":
     config = configparser.ConfigParser()
     config.read('HRAStationDataAnalysis/config.ini')
     date = config['PARAMETERS']['date']
+    date_processing = config['PARAMETERS']['date_processing']
 
 
     stations = [13, 14, 15, 17, 18, 19, 30]
 
     for station in stations:
-        cmd = f'python HRAStationDataAnalysis/C00_eventSearchCuts.py --stnID {station} --date {date}'
+        cmd = f'python HRAStationDataAnalysis/C00_eventSearchCuts.py --stnID {station} --date {date} --date_processing {date_processing}'
         A00_SlurmUtil.makeAndRunJob(cmd, f'{station}_eventSearchCuts', runDirectory='run/eventSearchCuts', partition='standard', n_cpus=2)
 
     # Also run without a station ID to process time overlaps
 
-    cmd = f'python HRAStationDataAnalysis/C00_eventSearchCuts.py --date {date}'
+    cmd = f'python HRAStationDataAnalysis/C00_eventSearchCuts.py --date {date} --date_processing {date_processing}'
     A00_SlurmUtil.makeAndRunJob(cmd, 'ALL_eventSearchCuts', runDirectory='run/ALL_eventSearchCuts', partition='standard', n_cpus=2)
