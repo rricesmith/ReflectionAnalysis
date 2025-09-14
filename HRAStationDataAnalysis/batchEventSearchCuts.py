c5aa0cac44f4182f5a1722fd3cc8c6695b0ce5b8
rricesmith <rricesmith@gmail.com>
2025-09-14 14:08:38 -0700

    Fix batch

--- HRAStationDataAnalysis/batchEventSearchCuts.py
+++ HRAStationDataAnalysis/batchEventSearchCuts.py
@@ -15,12 +15,12 @@ if __name__ == "__main__":
 
     stations = [13, 14, 15, 17, 18, 19, 30]
 
-    # for station in stations:
-    #     cmd = f'python HRAStationDataAnalysis/C00_eventSearchCuts.py --stnID {station} --date {date} --date_processing {date_processing}'
-    #     A00_SlurmUtil.makeAndRunJob(cmd, f'{station}_eventSearchCuts', runDirectory='run/eventSearchCuts', partition='standard', n_cpus=2, n_tasks=3)
+    for station in stations:
+        cmd = f'python HRAStationDataAnalysis/C00_eventSearchCuts.py --stnID {station} --date {date} --date_processing {date_processing}'
+        A00_SlurmUtil.makeAndRunJob(cmd, f'{station}_eventSearchCuts', runDirectory='run/eventSearchCuts', partition='standard', n_cpus=2, n_tasks=3)
 
     # Also run without a station ID to process time overlaps
-    # quit()
+    quit()
 
     cmd = f'python HRAStationDataAnalysis/C00_eventSearchCuts.py --date {date} --date_processing {date_processing}'
     A00_SlurmUtil.makeAndRunJob(cmd, 'ALL_eventSearchCuts', runDirectory='run/ALL_eventSearchCuts', partition='standard', n_cpus=2, n_tasks=3)
