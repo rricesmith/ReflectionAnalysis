3f91a37f4f4d79729e87429715aed7728ff27167
rricesmith <rricesmith@gmail.com>
2025-12-01 11:31:51 -0800

    Fix check

--- plotAmpResponse.py
+++ plotAmpResponse.py
@@ -34,14 +34,14 @@ sampling_rate = 2.0 # GHz
 dt = 1.0 / sampling_rate # ns
 t = np.arange(n_samples) * dt
 
-# Generate Noise
-# User specified: random rayleigh noise, sigma of 1.0
+# Generate Noise in Frequency Domain
+# Random complex noise (White noise in time, flat-ish in freq)
 np.random.seed(12345)
-noise_trace = np.random.rayleigh(scale=1.0, size=n_samples)
-
-# FFT
-fft_noise = np.fft.rfft(noise_trace)
 freqs = np.fft.rfftfreq(n_samples, d=dt) # GHz
+fft_noise = np.random.normal(0, 1, len(freqs)) + 1j * np.random.normal(0, 1, len(freqs))
+
+# Flat Value of 5 FFT (Comparison Signal)
+fft_flat = np.full_like(freqs, 5.0, dtype=complex)
 
 amplifiers = ['100', '200', '300']
 colors = {'100': 'purple', '200': 'darkorange', '300': 'darkgreen'}
@@ -53,15 +53,20 @@ for amp in amplifiers:
     gain_func = ac.amplifier_response[amp]['gain']
     response = gain_func(freqs)
     
-    # Convolve (multiply in freq domain)
-    fft_convolved = fft_noise * response
+    # Convolve Noise (multiply in freq domain)
+    fft_noise_convolved = fft_noise * response
+    trace_noise_convolved = np.fft.irfft(fft_noise_convolved, n=n_samples)
     
-    # Inverse FFT to get time trace
-    trace_convolved = np.fft.irfft(fft_convolved, n=n_samples)
+    # Convolve Flat-5 Signal
+    fft_flat_convolved = fft_flat * response
+    trace_flat_convolved = np.fft.irfft(fft_flat_convolved, n=n_samples)
     
-    # Plot Frequency Spectrum (Magnitude)
-    # No normalization
-    fft_mag = np.abs(fft_convolved)
+    # Determine limits from Flat-5 trace
+    # Use the maximum absolute value of the Flat-5 trace to set limits
+    max_limit = np.max(np.abs(trace_flat_convolved))
+    
+    # Plot Frequency Spectrum (Magnitude) of Noise
+    fft_mag = np.abs(fft_noise_convolved)
     
     ax_freq.plot(freqs, fft_mag, label=f'Noise', linestyle='dashed', color=colors[amp], linewidth=2)
     ax_freq.set_title(f'Frequency Spectrum - {amp}-series')
@@ -70,13 +75,15 @@ for amp in amplifiers:
     ax_freq.legend()
     ax_freq.grid(True)
 
-    # Plot Time Trace
-    # No normalization
-    ax_time.plot(t, trace_convolved, label=f'Noise', linestyle='dashed', color=colors[amp], linewidth=2)
+    # Plot Time Trace of Noise
+    ax_time.plot(t, trace_noise_convolved, label=f'Noise', linestyle='dashed', color=colors[amp], linewidth=2)
     ax_time.set_title(f'Time Trace - {amp}-series')
     ax_time.set_xlabel('Time [ns]')
     ax_time.set_ylabel('Amplitude')
-    ax_time.set_ylim(-5, 5)
+    
+    # Set limits based on Flat-5 signal
+    ax_time.set_ylim(-max_limit, max_limit)
+    
     ax_time.legend()
     ax_time.grid(True)
 
