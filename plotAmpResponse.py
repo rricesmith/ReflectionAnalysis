947f7eff749aa885817543d0fe0ff7e3be8c28ef
rricesmith <rricesmith@gmail.com>
2025-12-01 11:24:02 -0800

    Update amp plot

--- plotAmpResponse.py
+++ plotAmpResponse.py
@@ -27,7 +27,6 @@ fig.savefig('plots/ampResponse.png')
 #fig.savefig(PathToARIANNAanalysis + '/plots/ampResponse.pdf')
 
 # --- Noise Analysis ---
-fig2, (ax_freq, ax_time) = plt.subplots(2, 1, figsize=(12, 10))
 
 # Parameters
 n_samples = 256
@@ -37,6 +36,7 @@ t = np.arange(n_samples) * dt
 
 # Generate Noise
 # User specified: random rayleigh noise, sigma of 1.0
+np.random.seed(12345)
 noise_trace = np.random.rayleigh(scale=1.0, size=n_samples)
 
 # FFT
@@ -44,10 +44,11 @@ fft_noise = np.fft.rfft(noise_trace)
 freqs = np.fft.rfftfreq(n_samples, d=dt) # GHz
 
 amplifiers = ['100', '200', '300']
-styles = {'100': 'solid', '200': 'dotted', '300': 'dashed'}
-colors = {'100': 'darkblue', '200': 'darkorange', '300': 'darkgreen'}
+colors = {'100': 'purple', '200': 'darkorange', '300': 'darkgreen'}
 
 for amp in amplifiers:
+    fig_amp, (ax_freq, ax_time) = plt.subplots(2, 1, figsize=(12, 10))
+    
     # Get amplifier response
     gain_func = ac.amplifier_response[amp]['gain']
     response = gain_func(freqs)
@@ -58,32 +59,27 @@ for amp in amplifiers:
     # Inverse FFT to get time trace
     trace_convolved = np.fft.irfft(fft_convolved, n=n_samples)
     
-    # Renormalize FFT (magnitude)
+    # Plot Frequency Spectrum (Magnitude)
+    # No normalization
     fft_mag = np.abs(fft_convolved)
-    if np.max(fft_mag) != 0:
-        fft_mag /= np.max(fft_mag)
-        
-    # Renormalize Time Trace
-    if np.max(np.abs(trace_convolved)) != 0:
-        trace_convolved /= np.max(np.abs(trace_convolved))
-        
-    # Plot
-    ax_freq.plot(freqs, fft_mag, label=f'{amp}-series', linestyle=styles[amp], color=colors[amp], linewidth=2)
-    ax_time.plot(t, trace_convolved, label=f'{amp}-series', linestyle=styles[amp], color=colors[amp], linewidth=2)
+    
+    ax_freq.plot(freqs, fft_mag, label=f'Noise', linestyle='dashed', color=colors[amp], linewidth=2)
+    ax_freq.set_title(f'Frequency Spectrum - {amp}-series')
+    ax_freq.set_xlabel('Frequency [GHz]')
+    ax_freq.set_ylabel('Amplitude')
+    ax_freq.legend()
+    ax_freq.grid(True)
 
-ax_freq.set_title('Normalized Frequency Spectrum of Noise')
-ax_freq.set_xlabel('Frequency [GHz]')
-ax_freq.set_ylabel('Normalized Amplitude')
-ax_freq.legend()
-ax_freq.grid(True)
+    # Plot Time Trace
+    # No normalization
+    ax_time.plot(t, trace_convolved, label=f'Noise', linestyle='dashed', color=colors[amp], linewidth=2)
+    ax_time.set_title(f'Time Trace - {amp}-series')
+    ax_time.set_xlabel('Time [ns]')
+    ax_time.set_ylabel('Amplitude')
+    ax_time.set_ylim(-5, 5)
+    ax_time.legend()
+    ax_time.grid(True)
 
-ax_time.set_title('Normalized Time Trace of Noise')
-ax_time.set_xlabel('Time [ns]')
-ax_time.set_ylabel('Normalized Amplitude')
-ax_time.legend()
-ax_time.grid(True)
-
-fig2.tight_layout()
-
-# plt.show()
-fig2.savefig('plots/noiseAnalysis.png')
+    fig_amp.tight_layout()
+    fig_amp.savefig(f'plots/noiseAnalysis_{amp}.png')
+    plt.close(fig_amp)
